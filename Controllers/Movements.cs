9ea8a57be2025bf0ba6bf9a094b29559df295445
Alex Giraldo <giraldojhong4@gmail.com>
2026-06-08 16:57:30 -0500

    clean

---
+++ Controllers/Movements.cs
@@ -0,0 +1,57 @@
+﻿namespace LIN.Access.Notes.Controllers;
+
+public static class Movements
+{
+
+    public static async Task<ReadAllResponse<MovementDataModel>> ReadAll(string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("movements/read/all");
+
+        // Headers.
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Get<ReadAllResponse<MovementDataModel>>();
+
+        // Retornar.
+        return Content;
+
+    }
+    public static async Task<CreateResponse> Create(MovementDataModel model, string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("movements");
+
+        // Headers.
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Post<CreateResponse>(model);
+
+        // Retornar.
+        return Content;
+
+    }
+    public static async Task<ResponseBase> Delete(int id, string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("movements");
+
+        // Headers.
+        client.AddParameter("id", id);
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Delete<ResponseBase>();
+
+        // Retornar.
+        return Content;
+
+    }
+
+
+}
---
+++ Controllers/Tasks.cs
@@ -0,0 +1,60 @@
+﻿namespace LIN.Access.Notes.Controllers;
+
+public static class Tasks
+{
+
+    public static async Task<CreateResponse> Create(TaskDataModel model, string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("tasks");
+
+        // Headers.
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Post<CreateResponse>(model);
+
+        // Retornar.
+        return Content;
+
+    }
+
+    public static async Task<ResponseBase> Update(TaskDataModel model, string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("tasks");
+
+        // Headers.
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Patch<ResponseBase>(model);
+
+        // Retornar.
+        return Content;
+
+    }
+
+
+    public static async Task<ResponseBase> Delete(int id, string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("tasks");
+
+        // Headers.
+        client.AddParameter("id", id);
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Delete<ResponseBase>();
+
+        // Retornar.
+        return Content;
+
+    }
+
+
+}
